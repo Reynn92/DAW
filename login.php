9a451539b930973656f6f18bf77d3b1a47d0c44c
Borja Rodriguez <neokeit@gmail.com>
2024-05-24 17:13:35 +0200

    fichero con el menu para todas las ventanas

--- login.php
+++ login.php
@@ -84,77 +84,10 @@ if (isset($_GET['logout'])) {
 <body>
 
 <?php
-// Función para generar el menú
-function generarMenu()
-{
-    ?>
-    <link rel="stylesheet" href="css/EstiloDatos.css">
-    <header>
-    <link href="https://fonts.cdnfonts.com/css/metal-gear-solid" rel="stylesheet">    
+	include("menu.php");
 
-        <h2 class="cabeza">Biblioteca
-        <?php
-        if (isset($_SESSION['usuario'])) {
-            ?>
-            <div class="header-right">
-                <span><?php echo $_SESSION['usuario']; ?></span>
-                <form action="login.php" method="GET">
-                    <input type="hidden" name="logout" value="1">
-                    <input type="submit" value="Cerrar Sesión">
-                </form>
-            </div>
-        </h2>
-            <?php
-        }
-        ?>
-    </header>
-    <nav class="principal">
-        <ul class="nav">
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="">Plataformas</a>
-                <ul>
-                    <li><a href="datos.php">Gameboy</a></li>
-                    <li><a href="">NES</a></li>
-                    <li><a href="">SnES</a></li>
-                    <li><a href="">Nintendo64</a>
-                        <ul>
-                            <li><a href="">E-mail</a></li>
-                            <li><a href="">Telefono</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li><a href="">Sagas</a>
-                <ul>
-                    <li><a href="">Zelda</a></li>
-                    <li><a href="">Mario</a></li>
-                    <li><a href="">Final Fantasy</a></li>
-                    <li><a href="">Pokemon</a></li>
-                </ul>
-            </li>
-            <li><a href="juego.php">Añadir</a></li>
-            <li><a href="login.php">LogIn</a></li>
-            <li><a href="Nuevo.php">Registrate</a></li>
-            <?php
-            // Verificar si el usuario ha iniciado sesión
-            if (isset($_SESSION['usuario'])) {
-                ?>
-                <!-- <li>
-                    <form action="login.php" method="GET">
-                        <input type="hidden" name="logout" value="1">
-                        <input type="submit" value="Cerrar Sesión">
-                    </form>
-                </li> -->
-                <?php
-            }
-            ?>
-        </ul>
-    </nav>
-    <?php
-}
-
-// Llamar a la función para generar el menú
-generarMenu();
+	// Llamar a la función para generar el menú
+	generarMenu();
 ?>
 
 <div class="container">
